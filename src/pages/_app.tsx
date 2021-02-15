e8541b5eb330c8a362da17524616a21acbf3c65c
Zach Harris <zachbharris94@gmail.com>
2021-02-15 10:51:49 -0600

    reset again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,8 @@ import { AppProps } from 'next/app'
 // global css
 import 'normalize.css'
 
-// components
-import AppLayout from 'components/AppLayout'
-
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
-  )
+  return <Component {...pageProps} />
 }
 
 export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
 
-// components
-import Button from 'components/Shared/Button'
-
 function Home() {
-  return (
-    <div>
-      <Button icon="user">Primary</Button>
-      <Button icon="plus" color="secondary">
-        Secondary
-      </Button>
-    </div>
-  )
+  return <div>hello</div>
 }
 
 export default Home
